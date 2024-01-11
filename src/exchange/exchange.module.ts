5ba3a47ca90911c3c4f10aefc497679e1e3b8684
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-01-11 13:15:15 -0300

    creating exchange controller and your method convertAmount

--- src/exchange/exchange.module.ts
+++ src/exchange/exchange.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 import { CurrenciesModule } from 'src/currencies/currencies.module';
+import { ExchangeController } from './exchange.controller';
 
 @Module({
   imports: [CurrenciesModule],
   providers: [ExchangeService],
+  controllers: [ExchangeController],
 })
 export class ExchangeModule {}
